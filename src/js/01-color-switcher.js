2cdc96da5aafe9a9877ffc5170742d52255c09f1
Vitalii-Krat <kratvetal@ukr.net>
2021-11-02 14:17:36 +0200

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const btnStart = document.querySelector('button[data-start]')
+const btnStop = document.querySelector('button[data-stop]')
 
+let timerId = 0;
+
+btnStart.addEventListener('click', onButtonStart);
+btnStop.addEventListener('click', onButtonStop);
+
+function onButtonStart() {
+    btnStart.setAttribute('disabled', 'disabled')
+
+   timerId = setInterval(() => {
+        let bgColor = getRandomHexColor();
+        document.body.style.backgroundColor = bgColor;
+  console.log(" Run bg-color changing");
+    }, 1000);
+}
+
+function onButtonStop(){
+    clearInterval(timerId);
+    btnStart.removeAttribute('disabled');
+    console.log(`Interval with id ${timerId} has stopped!`);
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
