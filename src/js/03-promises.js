e73ea979e651b27f690a07cd59b04418b93d1d22
Vitalii-Krat <kratvetal@ukr.net>
2021-11-05 23:34:21 +0200

    add task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,36 @@
+import Notiflix from 'notiflix';
+
+const formPromise = document.querySelector('.form');
+formPromise.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(evt) {
+evt.preventDefault()
+  let { elements: { delay, step, amount },} = evt.currentTarget;
+delay = Number(delay.value);
+step = Number(step.value);
+amount = Number(amount.value);
+  
+  for (let position = 1; position <= amount; position++) {
+createPromise(position, delay)
+.then(({ position, delay }) => {
+    setTimeout(() => {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    }, delay);
+  })
+.catch(({ position, delay }) => {
+    setTimeout(() => {
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    }, delay);
+});
+delay += step;}};
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+  return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      resolve({ position, delay });
+    } else {
+      reject({ position, delay });
+    }
+})
+};
